fe049e6aff11aca16688fb0fa2921069180a6356
M-Hussain75 <focus.07591@gmail.com>
2025-01-15 13:52:35 +0500

    first commit

---
+++ python1.py
@@ -0,0 +1,59 @@
+print("Helllo World")  # basic print statement
+"""Hello
+how are you?
+Where are you from?"""
+print("I am Fine.\nI am from Lahore.")
+print("Pakistan Independence Date is")
+print(14,"August",1947, sep= "-")
+print("Hey", end="!") 
+a= 7
+b="Pakistan"
+c= None
+d= True
+e= complex(2,8)
+f= [1,2,3]
+g=(1,2,3)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5)
+print(5//5)
+print(5%5)
+print(5**5)
+h= 7
+i= 3.3
+print(h+i) #implicit conversion of data type
+j=75
+k="91"
+print(j+int(k)) #explicit conversion of data type
+l=input("Enter your age :")
+print(l)
+m='''Today I am trying to revise 
+my all python lectures it's bit 
+difficult but i will do it'''
+print(m)
+n="Hussain"
+print(n[3]) # to print a particular character from the string
+for character in n:   # to print all chartacter of the string
+    print(character)
+print(len(n)) # to find the length of string
+print(n[0:3])
+# string methods
+o="pakistan"
+p=o.upper()
+print(p)
+q="ZINDABAD"
+r=q.lower()
+print(r)
+s=q.index("D") #all string methods are avaiable on w3_school
+print(s)
+
+
+
---
+++ python2.py
@@ -0,0 +1,7 @@
+a=int(input("Enter your age : "))
+if a<18:
+    print("You are a teenager")
+elif a==18:
+    print("You are a youngester")
+elif a>=40:
+    print("You are asad khan")
