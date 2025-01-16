6a107667dc8b510a0b2ff7d03ceaa894a55bb1de
M-Hussain75 <focus.07591@gmail.com>
2025-01-16 13:47:49 +0500

    git push

--- python2.py
+++ python2.py
@@ -5,3 +5,17 @@ elif a==18:
     print("You are a youngester")
 elif a>=40:
     print("You are asad khan")
+
+#nested if statement
+b=int(input("Enter 1st number :"))
+c=int(input("Enter 2nd number :"))
+d=int(input("Enter 3rd number :"))
+if b>c:
+    if b>d:
+        print(b,"b is greatest")
+    else:
+        print(d,"d is greatest")
+elif c>d:
+    print(c,"c is greatest")
+else:
+    print(d,"d is greatest")
