783b880866d4bbeb299ceeaf4789d55aa07a13fd
M-Hussain75 <focus.07591@gmail.com>
2025-01-20 14:10:05 +0500

    use of loop

---
+++ main.py 1/python1.1.py
@@ -0,0 +1,10 @@
+# use of while loop
+a= int(input("Enter a number :"))
+i=1
+while(i<=10):
+    print(a,"x",i,"=",a*i)
+    i=i+1
+# use of for loop 
+b=int(input("Enter a number :"))
+for i in range(1,11):
+    print(b,"x",i,"=",b*i)
